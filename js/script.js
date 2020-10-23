25ec11ce085b94a87366b042b80c0c2eba93c247
Robert McShinsky <robert.mcshinsky@gmail.com>
2020-10-22 22:04:56 -0600

    style changes

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ console.log(data);
 
 
 fetchDogImages ();
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
